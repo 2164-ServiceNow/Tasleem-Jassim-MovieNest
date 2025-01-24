695397ff39ca82c571ea7c91f044b57a873d6884
Mohammed10Jassim <jassim.developer10@gmail.com>
2025-01-24 20:10:21 +0530

    Update signinController.js

--- signIn/signinController.js
+++ signIn/signinController.js
@@ -1,4 +1,4 @@
-// signInController.js
+
 
 app.controller('signin', ['$scope', '$http', '$location','$window', function($scope, $http, $location,$window) {
   $scope.email = '';
@@ -16,14 +16,14 @@ app.controller('signin', ['$scope', '$http', '$location','$window', function($sc
       .then(function(response) {
         console.log('Response:', response.data.message);
         if (response.data.message === 'Sign in successful') {
-          localStorage.setItem('loggedInUser', $scope.email); // Save the logged-in user's email in localStorage
+          localStorage.setItem('loggedInUser', $scope.email); 
           $window.open('/movieDashboard/dashboard.html', '_self');
         } else {
           alert('Unexpected response: ' + response.data.message);
         }
       })
       .catch(function(error) {
-        console.error('Error details:', error); // Log full error object
+        console.error('Error details:', error); 
         const errorMessage = (error.data && error.data.message) ? error.data.message : 
           (error.status === 404 ? 'Endpoint not found' : 'An error occurred');
         alert(errorMessage);
